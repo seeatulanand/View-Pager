293f278d9c1d5b6090cfcf90194cb99ee63089f5
Atul Anand <atulanandbhatt@gmail.com>
2023-07-21 16:37:28 +0530

    first commit

---
+++ app/src/main/java/com/onnet/viewpager/MainActivity.java
@@ -0,0 +1,48 @@
+package com.onnet.viewpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    AutoScrollViewPager viewPager;
+    Integer[] imageId = {R.drawable.ic_baseline_battery_charging_full_24, R.drawable.ic_outline_battery_alert_24, R.drawable.ic_baseline_battery_full_24, R.drawable.ic_outline_battery_alert_24};
+    String[] imagesName = {"image1","image2","image3","image4"};
+    TextView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        view=findViewById(R.id.text);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+    }
+
+    private void showDialog() {
+        Dialog dialog=new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.pager_layout);
+        viewPager = (AutoScrollViewPager) dialog.findViewById(R.id.viewpager);
+        viewPager.startAutoScroll();
+        viewPager.setInterval(3000);
+        viewPager.setCycle(true);
+        viewPager.setStopScrollWhenTouch(true);
+
+        PagerAdapter adapter = new CustomAdapter(MainActivity.this, imageId, imagesName);
+        viewPager.setAdapter(adapter);
+        dialog.show();
+    }
+}
